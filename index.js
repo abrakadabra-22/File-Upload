d58c22d8f603bf34f3fa5d65131188d08a393c5e
abrakadabra <abrakadabra-22@ukr.net>
2025-02-16 17:55:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-
-
-
 // Ensure uploads directory exists
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
